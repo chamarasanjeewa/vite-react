585479e13171c80558fd5f8ce7372f7abf93d8f1
Chamara sanjeewa <chamara@babyjourney.se>
2022-12-05 14:58:50 +0530

    removed env

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  console.log("env", import.meta.env.VITE_ENVIRONMENT);
+  console.log("env", import.meta.env.VITE_ENVIRONMENT2);
   console.log("updated...");
   return (
     <div className="App">
