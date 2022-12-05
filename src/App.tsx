26847935e5a3369859809abc2fbcdd6c54a184bb
Chamara sanjeewa <chamara@babyjourney.se>
2022-12-05 15:03:55 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  console.log("env", import.meta.env.VITE_ENVIRONMENT);
+  console.log("updated...");
   return (
     <div className="App">
       <div>
@@ -28,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
