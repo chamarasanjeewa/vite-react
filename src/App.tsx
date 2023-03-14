113f0c887000b67d54a381c70b557c6af1fb4d97
Chamara Sanjeewa <chamara.sanjeewa@gmail.com>
2023-03-14 12:43:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+
 function App() {
   const [count, setCount] = useState(0);
   console.log("env", import.meta.env.VITE_ENVIRONMENT2);
